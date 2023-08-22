341d62b99b07dca47b688f6412163ab3c5f27e9a
kvbxss <kubadownar444@gmail.com>
2023-08-22 23:18:34 +0200

    .

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import React, { FunctionComponent } from 'react';
+import styled from 'styled-components';
+
+const Header: FunctionComponent = () => {
+    return (
+        <>
+        </>
+    );
+}
