91a56aa55a32299b1b8c451712b9de192aff56df
kvbxss <kubadownar444@gmail.com>
2023-08-22 23:17:43 +0200

    app frame

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { styled } from 'styled-components';
 
 const App: FunctionComponent = () => {
-  const [count, setCount] = useState(0);
+  
 
   return (
     <Router>
