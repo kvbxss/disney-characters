a0c01d7691b7ea4c269b65f83ceff55720ce8995
kvbxss <kubadownar444@gmail.com>
2023-08-22 22:05:39 +0200

    app frame

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React, { FunctionComponent, useState } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { styled } from 'styled-components';
+
+const App: FunctionComponent = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <Router>
+      
+    </Router>
+  );
+}
+
+export default App;
+ 
